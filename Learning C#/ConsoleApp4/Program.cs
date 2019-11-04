7016f98bd979d93257a00ff66a09cf43f98f38e6
Marcin Bieniecki <marcin.bieniecki@starter24.pl>
2019-11-04 18:46:21 +0100

    first commit

---
+++ Learning C#/ConsoleApp4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string number = "1234";
+                byte b = Convert.ToByte(number);
+              
+                Console.WriteLine(b);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The nummber could not nonvert to a byte");
+            }
+
+
+
+
+        }
+    }
+}
