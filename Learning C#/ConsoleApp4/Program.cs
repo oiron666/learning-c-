0b9861e055890c05bea1d47f8e45c4e3f564210d
Marcin Bieniecki <marcin.bieniecki@starter24.pl>
2019-11-04 20:14:12 +0100

    keep uploading ranodm stuff

--- Learning C#/ConsoleApp4/Program.cs
+++ Learning C#/ConsoleApp4/Program.cs
@@ -6,9 +6,28 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp4
 {
-    class Program
+    class OperatorExercise
     {
         static void Main(string[] args)
+        {
+            //random stuff
+            var num1 = 3;
+            var num2 = 8;
+            
+            /*
+             *random stuff too
+             */
+            Console.WriteLine(num2 / num1);
+            Console.WriteLine((float)num2 / (float)num1);
+            Console.WriteLine(!((float)num1 == num1 && num1 > num2));
+            Console.ReadKey();
+        }
+
+    }
+
+    class convertExercise
+    {
+        static void Main2(string[] args)
         {
             try
             {
@@ -16,7 +35,7 @@ namespace ConsoleApp4
                 int number = Convert.ToInt32(strNumber);
                 byte byteNumber = (byte)number;
                 byte strToByteNumber = Convert.ToByte(strNumber);
-                string booleanString = 'false';
+                string booleanString = "false";
                 bool falseCheck = Convert.ToBoolean(booleanString);
                 Console.WriteLine(number + byteNumber + strToByteNumber);
                 Console.WriteLine(falseCheck);
@@ -25,11 +44,11 @@ namespace ConsoleApp4
             catch (Exception)
             {
                 Console.WriteLine("The nummber could not be converted to a byte");
+
             }
-
-
-
 
         }
     }
+
+
 }
